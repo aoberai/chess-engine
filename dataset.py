da30b53ad46ef8d2e1cc766229e6b975f87f14a3
aoberai <aoberai05@gmail.com>
2021-06-14 19:26:47 -0700

    made some modifications to model, not working

--- dataset.py
+++ dataset.py
@@ -10,55 +10,55 @@ board.push_san("e4")
 
 print(board)
 
-# app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def test():
+    print(chess.svg.board(board))
+    return render_template('index.html', svg=Markup(chess.svg.board(board, size = 500)))
+
+app.run()
+
+# piece_char_2_int = {"R" : 21, "N": 22, "B": 23, "Q" : 24, "K" : 25, "P" : 26, "r" : 11, "n": 12, "b": 13, "q" : 14, "k" : 15, "p" : 16, "." : 0} # Uppercase is Black, lowercase white. Conversion to int since model can only take float input, not string
 #
-# @app.route('/')
-# def test():
-#     print(chess.svg.board(board))
-#     return render_template('index.html', svg=Markup(chess.svg.board(board, size = 500)))
 #
-# app.run()
+# input_length = 92
+# fen_lengths = []
+# positions = []
+# evaluations = []
+# colors = []
+# with open("data/chessData.csv") as file:
+#     file.readline()
+#     line_number = 1
+#     for line in file.readlines():
+#         position_eval = line.split(",")
+#         #numpy position_eval[1] = float(position_eval[1].replace("\n", ""))
+#         normalized_evaluation = 1 if "#" in position_eval[1] else 2 * (1 / (1 + math.exp(-float(position_eval[1])/300)) - 0.5) # normalizes centipawn score with sigmoid function
+#         position_eval[0] = position_eval[0] + (input_length - len(position_eval[0])) * " " # Add padding on FEN
+#         position_array = np.asarray([i.split(" ") for i in str(chess.Board(position_eval[0])).split("\n")])
+#         position_array_int = np.zeros((8, 8))
+#         for i in range(0, len(position_array)):
+#             for j in range(0, len(position_array[i])):
+#                 position_array_int[i][j] = piece_char_2_int[position_array[i][j]]
+#         color = 1 if 'w' == position_eval[0].split()[1] else 0 # 1 if white, 0 if black to move
 #
-piece_char_2_int = {"R" : 21, "N": 22, "B": 23, "Q" : 24, "K" : 25, "P" : 26, "r" : 11, "n": 12, "b": 13, "q" : 14, "k" : 15, "p" : 16, "." : 0} # Uppercase is Black, lowercase white. Conversion to int since model can only take float input, not string
-
-
-input_length = 92
-fen_lengths = []
-positions = []
-evaluations = []
-colors = []
-with open("data/chessData.csv") as file:
-    file.readline()
-    line_number = 1
-    for line in file.readlines():
-        position_eval = line.split(",")
-        #numpy position_eval[1] = float(position_eval[1].replace("\n", ""))
-        normalized_evaluation = 1 if "#" in position_eval[1] else 2 * (1 / (1 + math.exp(-float(position_eval[1])/300)) - 0.5) # normalizes centipawn score with sigmoid function
-        position_eval[0] = position_eval[0] + (input_length - len(position_eval[0])) * " " # Add padding on FEN
-        position_array = np.asarray([i.split(" ") for i in str(chess.Board(position_eval[0])).split("\n")])
-        position_array_int = np.zeros((8, 8))
-        for i in range(0, len(position_array)):
-            for j in range(0, len(position_array[i])):
-                position_array_int[i][j] = piece_char_2_int[position_array[i][j]]
-        color = 1 if 'w' == position_eval[0].split()[1] else 0 # 1 if white, 0 if black to move
-
-        print(line_number)
-        print(position_eval)
-        print(chess.Board(position_eval[0]).unicode())
-        print(chess.Board(position_eval[0]))
-        print(position_array_int)
-        print("CentiPawn Score:", position_eval[1])
-        print("Normalized Evaluation:", normalized_evaluation)
-        print("Color:", color)
-
-        positions.append(position_array_int)
-        evaluations.append(normalized_evaluation)
-        colors.append(color)
-
-        line_number += 1
-        if line_number == 100000:
-            break
-# tf.keras.models.Sequential([tf.keras.layers.Conv2D(64, input_shape=input_length)])
-np.save("evaluations", np.asarray(evaluations))
-np.save("positions", np.asarray(positions))
-np.save("colors", np.asarray(colors))
+#         print(line_number)
+#         print(position_eval)
+#         print(chess.Board(position_eval[0]).unicode())
+#         print(chess.Board(position_eval[0]))
+#         print(position_array_int)
+#         print("CentiPawn Score:", position_eval[1])
+#         print("Normalized Evaluation:", normalized_evaluation)
+#         print("Color:", color)
+#
+#         positions.append(position_array_int)
+#         evaluations.append(normalized_evaluation)
+#         colors.append(color)
+#
+#         line_number += 1
+#         if line_number == 12000000:
+#             break
+# # tf.keras.models.Sequential([tf.keras.layers.Conv2D(64, input_shape=input_length)])
+# np.save("evaluations", np.asarray(evaluations))
+# np.save("positions", np.asarray(positions))
+# np.save("colors", np.asarray(colors))
--- scratch.py
+++ scratch.py
@@ -1,10 +1,31 @@
 import numpy as np
+import math
 
-board = "r n . q k b . r\n. . . . p p . p\n. P . . . n p .\np . p P p . . .\n. . . . . . . .\n. . N . . N . .\nP P . . . P P P\nR . B Q . K . R"
+# board = "r n . q k b . r\n. . . . p p . p\n. P . . . n p .\np . p P p . . .\n. . . . . . . .\n. . N . . N . .\nP P . . . P P P\nR . B Q . K . R"
+
+normalized_evaluation = 2 * (1 / (1 + math.exp(-float(130)/300)) - 0.5)
+print(normalized_evaluation)
+normalized_evaluation = 2 * (1 / (1 + math.exp(-float(-500)/300)) - 0.5)
+
+print(normalized_evaluation)
+
+
+# print(board)
+# print(np.asarray([i.split(" ") for i in board.split("\n")]))
+
+
+piece_char_2_int = {"R" : 21, "N": 22, "B": 23, "Q" : 24, "K" : 25, "P" : 26, "r" : 11, "n": 12, "b": 13, "q" : 14, "k" : 15, "p" : 16, "." : 0} # Uppercase is Black, lowercase white. Conversion to int since model can only take float input, not string
+
+inv_map = {v: k for k, v in piece_char_2_int.items()}
+
+print(inv_map)
+position = [[11,  0,  0, 14,  0, 11, 15,  0], [ 0, 16, 16,  0,  0, 16, 16, 16,], [16,  0, 12, 16, 13, 12,  0,  0], [ 0,  0, 13,  0, 16,  0,  0,  0], [ 0,  0,  0,  0, 26,  0,  0,  0], [ 0, 23, 26, 26,  0, 22,  0, 26],  [26, 26,  0, 22,  0, 26, 26,  0], [21,  0, 23, 24, 25,  0,  0, 21]]
+
+
+for i in range(0, len(position)):
+    for j in range(0, len(position[i])):
+        print(" ", inv_map[position[i][j]], end = ' ')
+    print("\n")
 
 
 
-
-
-print(board)
-print(np.asarray([i.split(" ") for i in board.split("\n")]))
